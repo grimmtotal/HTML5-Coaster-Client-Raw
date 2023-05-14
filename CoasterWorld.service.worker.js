32df1c7b02660da485c04b66c759bd6b4d8fd4e3
Grimmtotal <grimmtotal@gmail.com>
2023-05-14 09:12:12 -0700

    Update CoasterWorld.service.worker.js

--- CoasterWorld.service.worker.js
+++ CoasterWorld.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1684048204|10107794722";
+const CACHE_VERSION = "1684080683|91114530";
 const CACHE_PREFIX = "Open World-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "CoasterWorld.offline.html";
